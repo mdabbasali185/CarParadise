3a75fc4614ecbde8ffb90564289a7bfcd804e2bf
Md Abbas Ali <mdabbasali185@gmail.com>
2022-04-04 03:48:31 +0600

    rechart

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Line, LineChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const Dashboard = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [chart, setChart] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setChart(data));
+  }, []);
+  return (
+    <div className="container mt-5">
+      <LineChart width={750} height={450} data={chart}>
+        <Line dataKey={"investment"}></Line>
+        <Line dataKey={"revenue"}></Line>
+        <Line dataKey={"sell"}></Line>
+        <XAxis dataKey={"month"}></XAxis>
+        <YAxis></YAxis>
+        <Tooltip></Tooltip>
+      </LineChart>
+    </div>
+  );
 };
 
 export default Dashboard;
